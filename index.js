9961701ba03fd20cd7ffbd86db3cfc9b0b2ba921
douglch <douglchwm@gmail.com>
2022-07-13 15:19:35 +0800

    added scripts

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
+app.use(express.static('build'));
 
 let notes = [
   {
